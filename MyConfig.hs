0af28a811909d04681f34a31ca5d9cd0a036ff66
Wojciech Jasiński <ilikeheaps@gmail.com>
2019-10-28 09:42:32 +0100

    Add basic support for multimedia buttons

--- MyConfig.hs
+++ MyConfig.hs
@@ -247,4 +247,25 @@ mode "launcher" {
 }
 
 bindsym $mod+d mode "launcher"
+
+# Pulse Audio controls
+bindsym XF86AudioRaiseVolume exec --no-startup-id pactl set-sink-volume 0 +5%
+bindsym XF86AudioLowerVolume exec --no-startup-id pactl set-sink-volume 0 -5%
+bindsym XF86AudioMute exec --no-startup-id pactl set-sink-mute 0 toggle
+
+# Screen brightness controls
+# (ThinkPads handle on hardware level)
+# bindsym XF86MonBrightnessUp exec xbacklight -inc 20 # increase screen brightness
+# bindsym XF86MonBrightnessDown exec xbacklight -dec 20 # decrease screen brightness
+
+# Touchpad controls
+# (TODO make the script work)
+bindsym XF86TouchpadToggle exec touchpad-toggle.sh
+
+# Media player controls
+# (Do not have playerctl)
+# bindsym XF86AudioPlay exec playerctl play
+# bindsym XF86AudioPause exec playerctl pause
+# bindsym XF86AudioNext exec playerctl next
+# bindsym XF86AudioPrev exec playerctl previous
 -}

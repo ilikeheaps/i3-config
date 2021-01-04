37ced7d26c68cf1b2c899903572bb1b76620124f
Wojciech Jasiński <ilikeheaps@gmail.com>
2021-01-04 15:34:03 +0100

    Scripts: add a workaround for dmenu stealing focus

--- scripts/move-and-focus-workspace.sh
+++ scripts/move-and-focus-workspace.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# select-workspace.sh might steal focus (by dmenu window appearing over the cursor) so we mark the current container first
+i3-msg "mark _switching_"
+
 ws=$($(dirname "$0")/select-workspace.sh)
 
-i3-msg "move container to workspace $ws; workspace $ws"
+i3-msg "[con_mark=\"switching\"] focus; move container to workspace $ws; workspace $ws"

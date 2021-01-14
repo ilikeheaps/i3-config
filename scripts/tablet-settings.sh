1b5faa16a8625771d30e82effc3fd188dcaa071f
Wojciech Jasiński <ilikeheaps@gmail.com>
2021-01-14 13:07:15 +0100

    Update settings for a new tablet

--- scripts/tablet-settings.sh
+++ scripts/tablet-settings.sh
@@ -2,11 +2,11 @@
 
 # xinput device id/name; obtained from "xinput list", confirmed with "xinput test <id>"
 # numeric IDs seem to be somewhat randomly assigned on startup so better use name
-dev_id="HUION 420 Pen"
+dev_id="XP-PEN STAR G640 Pen stylus"
 
-# tablet area in mm (any unit is fine, the ratio is what's important)
-H=57
-W=102
+# tablet area in inches (any unit is fine, the ratio is what's important)
+W=6
+H=4
 
 # display area in pixels (any unit is fine, the ratio is what's important)
 X=3440

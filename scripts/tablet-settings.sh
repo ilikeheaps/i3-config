0533c9ef79ac691e04dcfe541f9e3b65324433d8
Wojciech Jasiński <ilikeheaps@gmail.com>
2021-07-10 14:23:21 +0200

    Update tablet settings: more precise dimensions

--- scripts/tablet-settings.sh
+++ scripts/tablet-settings.sh
@@ -4,9 +4,11 @@
 # numeric IDs seem to be somewhat randomly assigned on startup so better use name
 dev_id="XP-PEN STAR G640 Pen stylus"
 
-# tablet area in inches (any unit is fine, the ratio is what's important)
-W=6
-H=4
+# tablet area in points (any unit is fine, the ratio is what's important)
+W=32000
+H=20000
+# these points are actual maximum reported values (which drivers/etc later map to [0,1] range)
+# (btw actual area is 16x10cm, not 6x4 inches)
 
 # display area in pixels (any unit is fine, the ratio is what's important)
 X=3440
